b42462a1db11fe2e5a692b83efe0418af733df0f
Ben Jefferies <benjefferies@echosoft.uk>
2019-10-25 14:51:58 +0100

    Fix IT for rs gateway (#2)
    * Fix IT for rs gateway
    
    Tests now run successfully when running `mvn clean install` and in codefresh

---
+++ forgerock-openbanking-uk-aspsp-rs/forgerock-openbanking-uk-aspsp-rs-gateway/forgerock-openbanking-uk-aspsp-rs-gateway-server/src/test/java/com/forgerock/openbanking/aspsp/rs/ForgerockOpenbankingRsApiApplication.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2019 ForgeRock AS. All Rights Reserved
+ *
+ * Use of this code requires a commercial software license with ForgeRock AS.
+ * or with one of its affiliates. All use shall be exclusively subject
+ * to such license between the licensee and ForgeRock AS.
+ */
+package com.forgerock.openbanking.aspsp.rs;
+
+import brave.Tracer;
+import com.forgerock.openbanking.common.error.ErrorHandler;
+import com.forgerock.openbanking.common.services.security.FormValueSanitisationFilter;
+import com.forgerock.openbanking.common.services.security.JsonRequestSanitisiationFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.servlet.Filter;
+
+@SpringBootApplication
+@EnableSwagger2
+@EnableDiscoveryClient
+@EnableScheduling
+@EnableWebSecurity
+@ComponentScan(basePackages = {"com.forgerock"})
+@EnableMongoRepositories(basePackages = "com.forgerock")
+public class ForgerockOpenbankingRsApiApplication {
+
+    public static void main(String[] args) {
+        new SpringApplication(ForgerockOpenbankingRsApiApplication.class).run(args);
+    }
+
+    @Bean
+    public Filter jsonSanitisationFilter(ErrorHandler errorHandler, Tracer tracer) {
+        return new JsonRequestSanitisiationFilter(errorHandler, tracer);
+    }
+
+    @Bean
+    public Filter formSanitisationFilter(ErrorHandler errorHandler, Tracer tracer) {
+        return new FormValueSanitisationFilter(errorHandler, tracer);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
