2c864c769d4e0be029fe9afcc1dc2eb7e4fac44c
Ben Jefferies <benjefferies@echosoft.uk>
2019-10-25 17:13:09 +0100

    Enable all integration tests and build samples (#3)
    * Enable all IT and build sample docker images
    
    RS mock store and RS RCS tests running locally and in codefresh.
    Build docker images in codefresh for sample projects.
    
    * Generate files

---
+++ forgerock-openbanking-uk-aspsp-rs/forgerock-openbanking-uk-aspsp-rs-rcs/forgerock-openbanking-uk-aspsp-rs-rcs-server/src/test/java/com/forgerock/openbanking/aspsp/rs/rcs/ForgerockOpenbankingRsApplication.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2019 ForgeRock AS. All Rights Reserved
+ *
+ * Use of this code requires a commercial software license with ForgeRock AS.
+ * or with one of its affiliates. All use shall be exclusively subject
+ * to such license between the licensee and ForgeRock AS.
+ */
+package com.forgerock.openbanking.aspsp.rs.rcs;
+
+import brave.Tracer;
+import com.forgerock.openbanking.common.error.ErrorHandler;
+import com.forgerock.openbanking.common.services.security.FormValueSanitisationFilter;
+import com.forgerock.openbanking.common.services.security.JsonRequestSanitisiationFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.Filter;
+
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {"com.forgerock"})
+@EnableMongoRepositories(basePackages = "com.forgerock")
+public class ForgerockOpenbankingRsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForgerockOpenbankingRsApplication.class, args);
+    }
+
+    @Bean
+    public Filter jsonSanitisationFilter(ErrorHandler errorHandler, Tracer tracer) {
+        return new JsonRequestSanitisiationFilter(errorHandler, tracer);
+    }
+
+    @Bean
+    public Filter formSanitisationFilter(ErrorHandler errorHandler, Tracer tracer) {
+        return new FormValueSanitisationFilter(errorHandler, tracer);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
