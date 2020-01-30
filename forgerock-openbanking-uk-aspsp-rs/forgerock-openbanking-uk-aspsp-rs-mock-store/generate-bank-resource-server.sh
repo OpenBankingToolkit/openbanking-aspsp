4a9539ab781827f9b24437a7e8cb8578c1f3ec7c
Ben Jefferies <benjefferies@echosoft.uk>
2020-01-30 12:37:05 +0000

    Create OpenAPI specs of rs store (#111)
    * Create OpenAPI specs of rs store
    
    Still work in progress but wanted to share initial workings with the
    team. I reverse generated the swagger by enabling SpringFox swagger
    endpoints and then refactoring the various endpoints into the different
    specs. The specs need to live separately because the `swagger-codegen`
    tool uses the first part of the path to decide which java files to
    create, because the first path was `/openbanking` for all APIs it was
    creating one massive file.
    
    To tests run
    
    ```
    forgerock-openbanking-uk-aspsp-rs/forgerock-openbanking-uk-aspsp-rs-mock-store/generate-bank-resource-server.sh spring spring-mvc --group-id com.forgerock.openbanking -DuseBeanValidation=true -DinterfaceOnly=true
    ```
    
    See https://github.com/OpenBankingToolkit/openbanking-aspsp/issues/108
    for more information

---
+++ forgerock-openbanking-uk-aspsp-rs/forgerock-openbanking-uk-aspsp-rs-mock-store/generate-bank-resource-server.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+generate() {
+local output_dir=$1
+local spec=$2
+local language=$3
+local library=$4
+local config="${@:5}"
+  swagger-codegen generate -o ${output_dir} -i ${spec} -l ${language} --library ${library} $config
+}
+
+base_path=forgerock-openbanking-uk-aspsp-rs/forgerock-openbanking-uk-aspsp-rs-mock-store
+output_dir=$1
+language=$2
+library=$3
+config="${@:4}"
+
+if [[ -z ${output_dir+x} || -z ${language+x} || -z ${library+x} || -z ${config+x} ]]; then
+  echo "Missing arguments"
+  echo
+  echo "output: ${output_dir}"
+  echo "language: ${language}"
+  echo "library: ${library}"
+  echo "config: ${config}"
+  echo
+  echo "Example usage:"
+  echo "    generate-bank-resource-server.sh bank-rs-store spring spring-mvc --group-id com.forgerock.openbanking -DuseBeanValidation=true -DinterfaceOnly=true"
+  exit 1
+fi
+
+generate ${output_dir} ${base_path}/internal.json ${language} ${library} "--model-package com.forgerock.rs.store.models.internal --api-package=com.forgerock.rs.store.apis.internal ${config}"
+generate ${output_dir} ${base_path}/internal-account-payments.json ${language} ${library} "--model-package com.forgerock.rs.store.models.internal --api-package=com.forgerock.rs.store.apis.internal ${config}"
+generate ${output_dir} ${base_path}/openbanking-v1.1.json ${language} ${library} "--model-package com.forgerock.rs.store.models.v1_0 --api-package=com.forgerock.rs.store.apis.v1_0 ${config}"
+generate ${output_dir} ${base_path}/openbanking-v2.0.json ${language} ${library} "--model-package com.forgerock.rs.store.models.v2_0 --api-package=com.forgerock.rs.store.apis.v2_0 ${config}"
+generate ${output_dir} ${base_path}/openbanking-v3.0.json ${language} ${library} "--model-package com.forgerock.rs.store.models.v3_0 --api-package=com.forgerock.rs.store.apis.v3_0 ${config}"
+generate ${output_dir} ${base_path}/openbanking-v3.1.1.json ${language} ${library} "--model-package com.forgerock.rs.store.models.v3.1.1 --api-package=com.forgerock.rs.store.apis.v3_1_1 ${config}"
+generate ${output_dir} ${base_path}/openbanking-v3.1.2.json ${language} ${library} "--model-package com.forgerock.rs.store.models.v3_1_2 --api-package=com.forgerock.rs.store.apis.v3_1_2 ${config}"
+generate ${output_dir} ${base_path}/openbanking-v3.1.json ${language} ${library} "--model-package com.forgerock.rs.store.models.v3_1 --api-package=com.forgerock.rs.store.apis.v3_1 ${config}"
