9d5b955a38c7632d360290001eef514a7f6cd1d0
Matt Wills <matt.wills@forgerock.com>
2020-08-21 08:53:28 +0100

    Added controllers for v3.1.6 of the payments API (#272)

---
+++ forgerock-openbanking-uk-aspsp-rs/forgerock-openbanking-uk-aspsp-rs-mock-store/forgerock-openbanking-uk-aspsp-rs-mock-store-server/src/main/java/com/forgerock/openbanking/aspsp/rs/store/api/openbanking/payment/v3_1_6/domesticpayments/DomesticPaymentConsentsApiController.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2019 ForgeRock AS.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.forgerock.openbanking.aspsp.rs.store.api.openbanking.payment.v3_1_6.domesticpayments;
+
+import com.forgerock.openbanking.analytics.services.ConsentMetricService;
+import com.forgerock.openbanking.aspsp.rs.store.repository.TppRepository;
+import com.forgerock.openbanking.aspsp.rs.store.repository.v3_1_5.payments.DomesticConsent5Repository;
+import com.forgerock.openbanking.common.conf.discovery.ResourceLinkService;
+import com.forgerock.openbanking.common.services.openbanking.FundsAvailabilityService;
+import org.springframework.stereotype.Controller;
+
+@Controller("DomesticPaymentConsentsApiV3.1.6")
+public class DomesticPaymentConsentsApiController extends com.forgerock.openbanking.aspsp.rs.store.api.openbanking.payment.v3_1_5.domesticpayments.DomesticPaymentConsentsApiController implements DomesticPaymentConsentsApi {
+
+    public DomesticPaymentConsentsApiController(DomesticConsent5Repository domesticConsentRepository,
+                                                TppRepository tppRepository,
+                                                FundsAvailabilityService fundsAvailabilityService,
+                                                ResourceLinkService resourceLinkService,
+                                                ConsentMetricService consentMetricService) {
+        super(domesticConsentRepository, tppRepository, fundsAvailabilityService, resourceLinkService, consentMetricService);
+    }
+}
