5c211b60d24784f7933b9b69a4f260c5f3fc05c7
jorgesanchezperez <jorge.sanchez-perez@forgerock.com>
2021-03-17 17:24:08 +0000

    Added default values for data creator documents and accounts limits

--- forgerock-openbanking-uk-aspsp-rs/forgerock-openbanking-uk-aspsp-rs-mock-store/forgerock-openbanking-uk-aspsp-rs-mock-store-server/src/main/java/com/forgerock/openbanking/aspsp/rs/store/api/openbanking/data/DataCreator.java
+++ forgerock-openbanking-uk-aspsp-rs/forgerock-openbanking-uk-aspsp-rs-mock-store/forgerock-openbanking-uk-aspsp-rs-mock-store-server/src/main/java/com/forgerock/openbanking/aspsp/rs/store/api/openbanking/data/DataCreator.java
@@ -97,8 +97,8 @@ public class DataCreator {
                        FRTransactionRepository transactionRepository, FRStatementRepository statementRepository,
                        FRScheduledPaymentRepository scheduledPaymentRepository, FRPartyRepository partyRepository,
                        FROfferRepository offerRepository,
-                       @Value("${rs.data.upload.limit.documents}") Integer documentLimit,
-                       @Value("${rs.data.upload.limit.accounts}") Integer accountLimit) {
+                       @Value("${rs.data.upload.limit.documents:1000}") Integer documentLimit,
+                       @Value("${rs.data.upload.limit.accounts:100}") Integer accountLimit) {
         this.accountsRepository = accountsRepository;
         this.balanceRepository = balanceRepository;
         this.beneficiaryRepository = beneficiaryRepository;
