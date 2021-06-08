05139a123712b77d8ce4310f332d73a40cc7cf83
Matt Wills <matt.wills@forgerock.com>
2021-06-08 09:52:32 +0100

    36: Initial controllers and their interfaces for VRPs
    - There are no request parameters in v3.1.8 of the VRP swagger spec, meaning the generated code did not have any request body objects or headers. I have added the request body objects manually, but they may not e correct.
    - I assume this is a mistake in the swagger spec and have created this issue: https://github.com/OpenBankingUK/read-write-api-specs/issues/51
    - As per other controllers in the Read/Write API, a number of annotations (e.g. @PreAuthorize and @OpenBankingApi) need adding. I have inserted them in commented out sections, but certain things (e.g.  the scopes) may not be correct.
    
    Issue: https://github.com/OpenBankingToolkit/openbanking-toolkit/issues/36

---
+++ forgerock-openbanking-uk-aspsp-rs/forgerock-openbanking-uk-aspsp-rs-gateway/forgerock-openbanking-uk-aspsp-rs-gateway-server/src/main/java/com/forgerock/openbanking/aspsp/rs/api/vrp/v3_1_8/DomesticVrpConsentsApiController.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2019 ForgeRock AS.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.forgerock.openbanking.aspsp.rs.api.vrp.v3_1_8;
+
+import com.forgerock.openbanking.exceptions.OBErrorResponseException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import uk.org.openbanking.datamodel.vrp.OBDomesticVRPConsentRequest;
+import uk.org.openbanking.datamodel.vrp.OBDomesticVRPConsentResponse;
+import uk.org.openbanking.datamodel.vrp.OBVRPFundsConfirmationRequest;
+
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2021-05-27T13:44:23.551801+01:00[Europe/London]")
+
+@Controller("DomesticVrpConsentsApiControllerV3.1.8")
+public class DomesticVrpConsentsApiController implements DomesticVrpConsentsApi {
+
+    @Override
+    public ResponseEntity<OBDomesticVRPConsentResponse> createDomesticVrpConsent(
+            OBDomesticVRPConsentRequest domesticVRPConsentRequest
+    ) throws OBErrorResponseException {
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ResponseEntity<OBDomesticVRPConsentResponse> getDomesticVrpConsent() {
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteDomesticVrpConsent() {
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+
+    @Override
+    public ResponseEntity<Object> createDomesticVrpConsentsConsentFundsConfirmation(
+            OBVRPFundsConfirmationRequest vrpFundsConfirmationRequest
+    ) throws OBErrorResponseException {
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+}
